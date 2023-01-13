197a70bd703166556cb0bdc58eb9e30920d15050
BertrandMuh <sangwabertrand14@gmail.com>
2023-01-13 12:06:30 -0600

    work on the delete route

--- public/index.js
+++ public/index.js
@@ -7,29 +7,92 @@ const getElementById = (id) => {
     return document.getElementById(id)
 }
 
-let requestButton = getElementById('request-button')
+let requestButton = getElementById('request-button');
+let deleteButton = getElementById('delete-button')
 
 const getPokemon = async () => {
     let res = await fetch('/get_pokemon_data');
     let parseData = await res.json();
     let pokemonContainer = createElement('div');
     pokemonContainer.setAttribute('id', 'pokemon-container');
+    let pokemonName = getElementById('pokemon-name').value
 
-    parseData.forEach(element => {
+    for (let i = 0; i < parseData.length; i++) {
+        // console.log(parseData.length)
+        let element = parseData[i]
         let pokemonNamePara = createElement('h3');
         let anchorTag = createElement('a');
-        anchorTag.setAttribute('href', element.img);
+        anchorTag.setAttribute('href', element.img + '.jpg');
         anchorTag.setAttribute('target', '_blank')
-        anchorTag.textContent = element.name
-        pokemonNamePara.appendChild(anchorTag)
+        console.log(pokemonName)
+        if (pokemonName.toLowerCase() == element.name.toLowerCase() || pokemonName.toLowerCase() == 'all') {
+            anchorTag.textContent = element.name
+            pokemonNamePara.appendChild(anchorTag)
+            pokemonContainer.append(pokemonNamePara);
+        }
+        else if (pokemonName.toLowerCase() !== element.name.toLowerCase()) {
+            pokemonNamePara.textContent = 'The pokemon could not be found. Try again later.'
+            pokemonContainer.append(pokemonNamePara);
+            break
 
-        pokemonContainer.append(pokemonNamePara);
-    });
+        }
+
+    };
     let body = getElementById('body');
-    if (getElementById('pokemon-container') === null) {
+    if (getElementById('pokemon-container') === null && pokemonName !== null) {
+        body.appendChild(pokemonContainer)
+    }
+    else if (getElementById('pokemon-container') !== null && pokemonName !== null) {
+
+        body.removeChild(getElementById('pokemon-container'));
         body.appendChild(pokemonContainer)
     }
 
 }
 
+const deletePokemon = async () => {
+    //send a fecth request
+    let pokemonName = getElementById('pokemon-name').value
+    let res = await fetch(`/delete_pokemon`);
+    // format the response so it can be read
+    let parseData = await res.json();
+    let pokemonContainer = createElement('div');
+    pokemonContainer.setAttribute('id', 'pokemon-container');
+
+
+    for (let i = 0; i < parseData.length; i++) {
+        // console.log(parseData.length)
+        let element = parseData[i]
+        let pokemonNamePara = createElement('h3');
+        let anchorTag = createElement('a');
+        anchorTag.setAttribute('href', element.img + '.jpg');
+        anchorTag.setAttribute('target', '_blank')
+        console.log(pokemonName);
+        anchorTag.textContent = element.name
+        pokemonNamePara.appendChild(anchorTag)
+        pokemonContainer.append(pokemonNamePara);
+        // if (pokemonName.toLowerCase() == element.name.toLowerCase() || pokemonName.toLowerCase() == 'all') {
+        //     anchorTag.textContent = element.name
+        //     pokemonNamePara.appendChild(anchorTag)
+        //     pokemonContainer.append(pokemonNamePara);
+        // }
+        // else if (pokemonName.toLowerCase() !== element.name.toLowerCase()) {
+        //     pokemonNamePara.textContent = 'The pokemon could not be found. Try again later.'
+        //     pokemonContainer.append(pokemonNamePara);
+        //     break
+
+        // }
+
+    };
+    let body = getElementById('body');
+    if (getElementById('pokemon-container') === null && pokemonName !== null) {
+        body.appendChild(pokemonContainer)
+    }
+    else if (getElementById('pokemon-container') !== null && pokemonName !== null) {
+        body.removeChild(getElementById('pokemon-container'));
+        body.appendChild(pokemonContainer)
+    }
+
+}
 requestButton.addEventListener('click', getPokemon);
+deleteButton.addEventListener('click', deletePokemon)
--- server.js
+++ server.js
@@ -1,20 +1,16 @@
 const express = require('express')
 const pokemon = require('./pokemon')
 
+
 const app = express()
 
-const createElement = (element) => {
-    return document.createElement(element)
-}
-
-const getElementById = (id) => {
-    return document.getElementById(id)
-}
-// Have the server feed you html file in the public folder
 app.use(express.static('public'))
 
 app.get('/get_pokemon_data', (req, res) => {
     res.send(pokemon)
+});
+app.delete('/delete_pokemon', (req, res) => {
+    res.send({ data: 'working' })
 });
 
 // have the server listen to a port
